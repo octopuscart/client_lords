7571e8849a5924197563f1108e3d03801e27cb8e
dev <dev@develop.com>
2019-06-13 14:44:28 +0530

    cahnges

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -93,7 +93,8 @@
 }
 
         </style>
-
+<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-9bfed4d8-7eb3-4134-a09d-a741b88f28d7"></div>
 
         <!-- Page Wrapper -->
         <div id="wrap" class="shop-page" ng-controller="ShopController"> 
