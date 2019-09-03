d7162b5ccdb97a6846cc22483b1259af69409949
dev <dev@develop.com>
2019-09-03 17:11:17 +0530

    changesin hdear

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -47,7 +47,7 @@
                     <li><a href="<?php echo site_url('Product/ProductList/4/0'); ?>">Sport Coats</a></li>
                     <li><a href="#">Waist Coats</a></li>
                     <li><a href="<?php echo site_url('Product/ProductList/3/0'); ?>">Slacks</a></li>
-                    <li><a href="#">Tuxedos</a></li>
+                    <li><a href="<?php echo site_url('Product/ProductList/5/0'); ?>">Tuxedos</a></li>
 
                 </ul>
             </div>
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -154,7 +154,7 @@
                                                 <li><a href="<?php echo site_url('Product/ProductList/4/0'); ?>">Sport Coats</a></li>
                                                 <li><a href="#">Waist Coats</a></li>
                                                 <li><a href="<?php echo site_url('Product/ProductList/3/0'); ?>">Slacks</a></li>
-                                                <li><a href="<?php echo site_url('Product/ProductList/8/0'); ?>">Tuxedos</a></li>
+                                                <li><a href="<?php echo site_url('Product/ProductList/5/0'); ?>">Tuxedos</a></li>
 
                                             </ul>
                                         </li>
