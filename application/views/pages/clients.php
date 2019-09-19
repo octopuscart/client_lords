62d7af02c956047df90d2d40c269b663599283a9
dev <dev@develop.com>
2019-09-19 16:04:20 +0530

    cahnes

--- application/views/pages/clients.php
+++ application/views/pages/clients.php
@@ -63,6 +63,15 @@ This time he's going to make me a new dress.",
         "tag" => "‎Courtney Clark (Austin, USA)"
     ),
     
+    array(
+        "images" => "paulterwal",
+        "details"=>"Paul ter Wal, President of the Global Speakers Federation and I do not only share the taste for cold beverages and travelling the world, but also trust the same tailor, Lord's Custom Tailors from Hong Kong.
+
+Big shout out to Jason Mirpuri! Thanks for always making us look great, pal ",
+          
+          "tag" => "‎Ilja Gee With Paul Ter Wal (President of the Global Speakers Federation)"
+    ),
+    
 ];
 
 
@@ -117,6 +126,13 @@ Great jacket has just arrived from Lord’s Custom Taylors. Perfect fit and exac
         "details" => "Lord's Custom Tailors sponsored the Professional Speakers Australia Convention in Brisbane, Australia recently. We met many amazing professional conference speakers and heard some amazing speeches. We also got to catch up with Lindsay Adams, the lucky winner of one of our tailored suits. That's him on the far left wearing a Lord's creation.",
         "tag" => "‎(Brisbane, Australia)"
     ),
+    
+      
+    
+    
+    
+    
+    
     
     
 ];
